5dc03becc21cc42a2910c46213d6623aadc311e3
Aaron Culich <aculich@gmail.com>
2017-01-25 21:10:43 -0800

    change to port 80 due to glitch in Jetstream networking somewhere with port 1723

--- neuro-startup.sh
+++ neuro-startup.sh
@@ -6,5 +6,5 @@ mkdir -p /home/aculich/work
 chown aculich:aculich /home/aculich/work
 docker pull aculich/wip-neuro
 docker rm -f wip-neuro
-docker run --name wip-neuro -p 1723:8888 -v /home/aculich/work:/home/jovyan/work -e GRANT_SUDO=yes  -d aculich/wip-neuro start-notebook.sh --NotebookApp.password='sha1:06dbcd27b65a:c4605b48a6a554d19ad61d99d427d809772a1baf'
+docker run --name wip-neuro -p 80:8888 -v /home/aculich/work:/home/jovyan/work -e GRANT_SUDO=yes  -d aculich/wip-neuro start-notebook.sh --NotebookApp.password='sha1:06dbcd27b65a:c4605b48a6a554d19ad61d99d427d809772a1baf'
 docker exec -it wip-neuro /home/jovyan/fetch_data.py
