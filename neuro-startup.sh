1168e05a1a1ff527ddbf00a2d62fc8b4c5819943
Aaron Culich <aculich@gmail.com>
2017-01-25 20:41:41 -0800

    fetchdata

--- neuro-startup.sh
+++ neuro-startup.sh
@@ -7,3 +7,4 @@ chown aculich:aculich /home/aculich/work
 docker pull aculich/wip-neuro
 docker rm -f wip-neuro
 docker run --name wip-neuro -p 1723:8888 -v /home/aculich/work:/home/jovyan/work -e GRANT_SUDO=yes  -d aculich/wip-neuro start-notebook.sh --NotebookApp.password='sha1:06dbcd27b65a:c4605b48a6a554d19ad61d99d427d809772a1baf'
+docker exec -it wip-neuro /home/jovyan/fetch_data.py
